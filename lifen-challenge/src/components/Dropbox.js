ee771fff63a87890622a86e005e2e3929c1f99bb
Virginie Beernaert <vbeernaert@yahoo.fr>
2019-08-12 21:49:29 +0200

    add more files onDrop instead of replacing

--- lifen-challenge/src/components/Dropbox.js
+++ lifen-challenge/src/components/Dropbox.js
@@ -1,29 +1,46 @@
-import React from "react";
-import { useDropzone } from "react-dropzone";
+import React, { Component } from "react";
+import Dropzone from "react-dropzone";
 
-function Dropbox(props) {
-  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
-  const files = acceptedFiles.map(file => (
-    <li key={file.path}>
-      {file.path} - {file.size} bytes
-    </li>
-  ));
-  return (
-    <div className="drop-container">
-      <div {...getRootProps({ className: "drop-dialog-box" })}>
-        <input {...getInputProps()} />
-        <h2>
-          <i class="fas fa-cloud-upload-alt" />
-        </h2>
-        <h3>Drag 'n' drop some files here</h3>
-        <h4>or click to select files</h4>
-      </div>
-      <aside className="files-name">
-        <h4>Files</h4>
-        <ul>{files}</ul>
-      </aside>
-    </div>
-  );
+class Dropbox extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      files: []
+    };
+  }
+
+  onDrop = files => {
+    let newFiles = this.state.files.concat(files);
+    this.setState({ files: newFiles });
+  };
+
+  render() {
+    const files = this.state.files.map(file => (
+      <li key={file.path}>
+        {file.path} - {file.size} bytes
+      </li>
+    ));
+    return (
+      <Dropzone onDrop={this.onDrop}>
+        {({ getRootProps, getInputProps }) => (
+          <div className="drop-container">
+            <div {...getRootProps({ className: "drop-dialog-box" })}>
+              <input {...getInputProps()} />
+              <h2>
+                <i class="fas fa-cloud-upload-alt" />
+              </h2>
+              <h3>Drag 'n' drop some files here</h3>
+              <h4>or click to select files</h4>
+            </div>
+            <aside className="files-name">
+              <h4>Files</h4>
+              <ul>{files}</ul>
+            </aside>
+          </div>
+        )}
+      </Dropzone>
+    );
+  }
 }
 
 export default Dropbox;
